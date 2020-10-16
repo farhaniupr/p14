cb5f313bb0dc3e985a5291434d177d9c12c45b83
farhaniupr <40320630+farhaniupr@users.noreply.github.com>
2020-10-16 09:47:24 +0700

    Add files via upload

---
+++ Server/serverGate.go
@@ -0,0 +1,273 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+var jwtKey = []byte("key")
+
+type Credentials struct {
+	Password string `json:"password"`
+	Username string `json:"username"`
+}
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+type News struct {
+	Status       string `json:"status"`
+	TotalResults int    `json:"totalResults"`
+	Articles     []struct {
+		Source struct {
+			ID   string `json:"id"`
+			Name string `json:"name"`
+		} `json:"source"`
+		Author      string    `json:"author"`
+		Title       string    `json:"title"`
+		Description string    `json:"description"`
+		URL         string    `json:"url"`
+		URLToImage  string    `json:"urlToImage"`
+		PublishedAt time.Time `json:"publishedAt"`
+		Content     string    `json:"content"`
+	} `json:"articles"`
+}
+
+type Data struct {
+	Rows []struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	} `json:"rows"`
+}
+
+//username dan password
+var users = map[string]string{
+	"farhani": "farhan",
+}
+
+//memanggil server 1 dengan token login
+func Server1(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("tokenJWT")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	tknStr := c.Value
+	claims := &Claims{}
+
+	tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if !tkn.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+
+	}
+
+	// panggil server 1
+	url := "http://localhost:54321/getNews"
+	req, _ := http.NewRequest("GET", url, nil)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	var aa News
+	json.Unmarshal(body, &aa)
+	json.NewEncoder(w).Encode(&aa)
+}
+
+//memanggil server 2 dengan token login
+func Server2(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("tokenJWT")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	tknStr := c.Value
+	claims := &Claims{}
+
+	tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if !tkn.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+
+	}
+
+	// panggil server 2
+	url := "http://localhost:12345/list"
+	req, _ := http.NewRequest("GET", url, nil)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	var dd Data
+	json.Unmarshal(body, &dd)
+	json.NewEncoder(w).Encode(dd.Rows)
+
+}
+
+//fungsi untuk masuk ke service dengan mendapatkan token
+func Signin(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	expectedPassword, ok := users[creds.Username]
+
+	if !ok || expectedPassword != creds.Password {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	expirationTime := time.Now().Add(5 * time.Minute) //waktu token login 5 menit
+	claims := &Claims{
+		Username: creds.Username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:    "tokenJWT",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
+}
+
+//fungsi yang memastikan bahwa berhasil masuk dengan token
+func Welcome(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("tokenJWT")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	tknStr := c.Value
+	claims := &Claims{}
+
+	tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if !tkn.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	w.Write([]byte(fmt.Sprintf("Selamat Datang %s!", claims.Username)))
+}
+
+/*func Refresh(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("tokenJWT")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	tknStr := c.Value
+	claims := &Claims{}
+	tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if !tkn.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	if time.Unix(claims.ExpiresAt, 0).Sub(time.Now()) > 30*time.Second {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	expirationTime := time.Now().Add(5 * time.Minute)
+	claims.ExpiresAt = expirationTime.Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	http.SetCookie(w, &http.Cookie{
+		Name:    "tokenJWT",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
+}*/
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/Server1", Server1).Methods("GET") //masuk ke server  1 perlu sign in
+	router.HandleFunc("/Server2", Server2).Methods("GET") //masuk ke server 2  perlu sign in
+	router.HandleFunc("/signin", Signin).Methods("POST")  //sign in mendapatkan token
+	router.HandleFunc("/welcome", Welcome).Methods("GET") //melakukan pengecekkan token berhasil didapatkan
+	http.Handle("/", router)
+	log.Fatal(http.ListenAndServe(":9000", router))
+}
